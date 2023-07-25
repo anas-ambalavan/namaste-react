2085e51f29454722fc4adf2d34f8b6423a75e5b9
Anas <17gcs23@meaec.edu.in>
2023-07-25 10:19:38 +0530

    Header

--- App.js
+++ App.js
@@ -1,43 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { UserCircleIcon } from "@heroicons/react/24/solid";
+import {
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  InformationCircleIcon,
+  MegaphoneIcon,
+  ShoppingBagIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
 
 import Logo from "./assets/logo.png";
 
-const SubTitle = <h3>Namaste React - React Element</h3>;
-
-const Title = () => (
-  <div id="title" className="title" tabIndex={1}>
-    <h1>Welcome!</h1>
-    {SubTitle}
-  </div>
-);
-
 const Header = () => (
-  <div className="header-container">
-    <div className="image-container">
-      <img
-        className="image"
-        width={"100%"}
-        height={"100%"}
-        src={Logo}
-        alt="logo"
-      />
-    </div>
-    <input className="search-bar" type="text" placeholder="Search..." />
-    <div className="section-right">
-      <UserCircleIcon width={30} />
+  <div className="header fonts-loaded">
+    <div className="header-container">
+      <div className="section-left">
+        <div className="logo-container">
+          <img
+            className="logo"
+            width={"100%"}
+            height={"100%"}
+            src={Logo}
+            alt="logo"
+          />
+        </div>
+        <div className="current-location">
+          <p>
+            <span style={{ fontWeight: "bold", textDecoration: "underline" }}>
+              BTM Layout
+            </span>{" "}
+            <span style={{ marginLeft: 5, color: "#686B78" }}>
+              Bengaluru, Karnataka, India
+            </span>
+          </p>
+          <ChevronDownIcon
+            style={{ marginLeft: 5, color: "#FC8018", cursor: "pointer" }}
+            width={20}
+          />
+        </div>
+      </div>
+      <div className="section-right">
+        <div className="header-list-item">
+          <MagnifyingGlassIcon width={20} />
+          <p>Search</p>
+        </div>
+        <div className="header-list-item">
+          <MegaphoneIcon width={20} />
+          <p>Offers</p>
+        </div>
+        <div className="header-list-item">
+          <InformationCircleIcon width={20} />
+          <p>Help</p>
+        </div>
+        <div className="header-list-item">
+          <UserCircleIcon width={20} />
+          <p>Sign In</p>
+        </div>
+        <div className="header-list-item">
+          <ShoppingBagIcon width={20} />
+          <p>Cart</p>
+        </div>
+      </div>
     </div>
   </div>
 );
 
-const Main = () => (
+const AppLayout = () => (
   <>
     <Header />
-    <Title />
   </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Main />);
+root.render(<AppLayout />);
