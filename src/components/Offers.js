48b634eb89748070684f8770ece92f5f34227c7c
Anas <17gcs23@meaec.edu.in>
2023-08-04 14:53:32 +0530

    disabled tailwind default presets

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -1,9 +1,9 @@
-import "../../tailwind.index.css";
+// import "../../tailwind.index.css";
 
 const Offers = () => {
   return (
-    <div className="mt-[5%] ml-[15%]">
-      <h1 className="font-bold text-2xl">
+    <div style={{ marginTop: "5%", marginLeft: "10%" }}>
+      <h1 style={{ fontSize: 20, fontWeight: 600 }}>
         Restaurants With Great Offers Near Me
       </h1>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [],
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
