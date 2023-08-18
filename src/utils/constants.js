2e931a26d2fbbb1e930bf0951b6ca1e2b23414a1
Anas <17gcs23@meaec.edu.in>
2023-08-18 14:04:22 +0530

    corsproxy added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,26 +1,26 @@
 export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+  "https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9165757&lng=77.6101163&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9165757&lng=77.6101163&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9165757&lng=77.6101163&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9165757&lng=77.6101163&restaurantId=";
 
 export const SUPPORT_PARTNER_API =
-  "https://www.swiggy.com/dapi/support/issues/partner-onboarding?";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/support/issues/partner-onboarding?";
 
 export const SUPPORT_LEGAL_API =
-  "https://www.swiggy.com/dapi/support/issues/legal?";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/support/issues/legal?";
 
 export const SUPPORT_FAQ_API =
-  "https://www.swiggy.com/dapi/support/issues/faq?";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/support/issues/faq?";
 
 export const PRE_SEARCH_API =
-  "https://www.swiggy.com/dapi/landing/PRE_SEARCH?lat=12.9165757&lng=77.6101163";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/landing/PRE_SEARCH?lat=12.9165757&lng=77.6101163";
 
 export const SEARCH_API =
-  "https://www.swiggy.com/dapi/restaurants/search/suggest?lat=12.9165757&lng=77.6101163&str=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/search/suggest?lat=12.9165757&lng=77.6101163&str=";
 
 export const SupportURLType = Object.freeze({
   partner: "partner-onboarding",
